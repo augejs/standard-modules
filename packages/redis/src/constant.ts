98dbda8601e66521a7bf8dbe2bb1aa3d401e640c
alex.zhang <gzane24@hotmail.com>
2021-03-21 00:57:50 +0800

    feat: new ver of 1.0.6

--- packages/redis/src/constant.ts
+++ packages/redis/src/constant.ts
@@ -1,3 +1,3 @@
 export const ConfigName = 'redis';
 export const REDIS_IDENTIFIER = Symbol.for(ConfigName);
-export const REDIS_SUBSCRIBER_IDENTIFIER = Symbol.for(ConfigName);
+export const REDIS_SUBSCRIBER_IDENTIFIER = Symbol.for(ConfigName + 'subscriber');
